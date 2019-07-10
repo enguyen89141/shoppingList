feec5af1614610b57d90fa29ecfc8cbf320da91f
Eric Nguyen <enguyen89141@gmail.com>
2019-07-09 19:21:20 -0700

    EN

---
+++ index.js
@@ -0,0 +1,34 @@
+function checked() {
+    $('.shopping-list').on('click', ".shopping-item-toggle",function(event) {
+        $(this).closest('li').find(".shopping-item").toggleClass("shopping-item__checked")
+    });
+  }
+
+
+
+function deleteItem() {
+    $('.shopping-list').on('click', ".shopping-item-delete", function(event) {
+        $(this).closest('li').remove()
+    });
+}
+function addItem() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const item = $(this).find('#shopping-list-entry').val()
+        $(".shopping-list").append(`<li>
+        <span class="shopping-item">` + item + `</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+      $('#shopping-list-entry').val('');
+    });
+}
+$(addItem);
+$(deleteItem);
+$(checked);
